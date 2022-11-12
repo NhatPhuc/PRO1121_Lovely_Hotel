6bd3fe8b603a4574856a04cba3897fb1466032d4
tran van trong <trongtvph21826@fpt.edu.vn>
2022-11-12 15:42:57 +0700

    layout_khung

---
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Adapter/viewpagerAdapter.java
@@ -0,0 +1,36 @@
+package phucnph22239.poly.lovely_hotel.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import phucnph22239.poly.lovely_hotel.Fragment.Fragment1;
+import phucnph22239.poly.lovely_hotel.Fragment.Fragment2;
+import phucnph22239.poly.lovely_hotel.Fragment.Fragment3;
+
+public class viewpagerAdapter extends FragmentStatePagerAdapter {
+    public viewpagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+            default:
+                return new Fragment1();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
