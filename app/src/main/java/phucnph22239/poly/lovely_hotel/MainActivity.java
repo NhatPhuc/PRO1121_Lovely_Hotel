30882b188d18d0bc78e174bf9c7f330c6f7d5395
NhatPhuc <101563029+NhatPhuc@users.noreply.github.com>
2022-11-12 13:43:32 +0700

    Init Project

---
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/MainActivity.java
@@ -0,0 +1,14 @@
+package phucnph22239.poly.lovely_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
