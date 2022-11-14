cfb7416f1d0ec17b8e53f5e00e6b2f997d4d77f8
NhatPhuc <101563029+NhatPhuc@users.noreply.github.com>
2022-11-14 23:33:25 +0700

    Update Doi Mat Khau

--- app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/FragmentDoiMatKhau.java
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/FragmentDoiMatKhau.java
@@ -10,14 +10,14 @@ import androidx.fragment.app.Fragment;
 import phucnph22239.poly.lovely_hotel.R;
 
 
-public class Fragment3 extends Fragment {
-    public Fragment3() {
+public class FragmentDoiMatKhau extends Fragment {
+    public FragmentDoiMatKhau() {
 
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        return inflater.inflate(R.layout.fragment_doimatkhau, container, false);
     }
 }
