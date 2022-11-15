836e76162ec0039176edc3b6ce87445ec648b462
tran van trong <trongtvph21826@fpt.edu.vn>
2022-11-15 12:56:03 +0700

    trong_layout_dv_kh

---
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/Fragment_don_dich_vu.java
@@ -0,0 +1,29 @@
+package phucnph22239.poly.lovely_hotel.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import phucnph22239.poly.lovely_hotel.R;
+
+
+public class Fragment_don_dich_vu extends Fragment {
+
+
+    public Fragment_don_dich_vu() {
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_don_dich_vu, container, false);
+    }
+}
---
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/Fragment_khachhang.java
@@ -0,0 +1,24 @@
+package phucnph22239.poly.lovely_hotel.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import phucnph22239.poly.lovely_hotel.R;
+
+public class Fragment_khachhang extends Fragment {
+
+    public Fragment_khachhang() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_khachhang, container, false);
+    }
+}
