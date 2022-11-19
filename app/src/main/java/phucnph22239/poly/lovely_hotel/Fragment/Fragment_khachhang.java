648cae85b3beb7bd1aa8497e405ae96c1ca760d3
Nguyen chi sang <sangncph25176@fpt.edu.vn>
2022-11-19 20:52:44 +0700

    sáng thêm khách hàng

--- app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/Fragment_khachhang.java
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/Fragment_khachhang.java
@@ -1,5 +1,6 @@
 package phucnph22239.poly.lovely_hotel.Fragment;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,13 +12,17 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -25,7 +30,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import phucnph22239.poly.lovely_hotel.Adapter.KhachHangAdapter;
 import phucnph22239.poly.lovely_hotel.DAO.KhachHangDAO;
@@ -49,6 +56,7 @@ public class Fragment_khachhang extends Fragment {
     KhachHangAdapter adapter;
     ArrayList<KhachHang> list ;
     FloatingActionButton btnADD;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -60,6 +68,7 @@ public class Fragment_khachhang extends Fragment {
         adapter = new KhachHangAdapter(getContext(),list);
         rcv_kh.setAdapter(adapter);
         btnADD = view.findViewById(R.id.btn_add_khach_hang);
+        Calendar calendar =Calendar.getInstance();
         btnADD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,11 +81,31 @@ public class Fragment_khachhang extends Fragment {
                 }
                 window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
                 window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                WindowManager.LayoutParams windowacc = window.getAttributes();
+                windowacc.gravity = Gravity.NO_GRAVITY ;
+                window.setAttributes(windowacc);
                 Button btnCancel = dialog.findViewById(R.id.btnCancel);
                 Button btnAdd = dialog.findViewById(R.id.btnAdd_KH);
                 EditText ed_name = dialog.findViewById(R.id.edName);
                 EditText ed_phone = dialog.findViewById(R.id.edPhone);
                 EditText ed_birthday = dialog.findViewById(R.id.edBirthday);
+                ImageView img = dialog.findViewById(R.id.imgdate);
+                img.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatePickerDialog dialog1 = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                int myear = year ;
+                                int mmonth = month ;
+                                int mdayOfMonth = dayOfMonth ;
+                                GregorianCalendar c = new GregorianCalendar(myear,mmonth,mdayOfMonth);
+                                    ed_birthday.setText(sdf.format(c.getTime()));
+                            }
+                        },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DATE));
+                        dialog1.show();
+                    }
+                });
                 btnAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -84,7 +113,7 @@ public class Fragment_khachhang extends Fragment {
                                 ed_phone.getText().length()==0||
                                 ed_birthday.getText().length()==0){
                             Toast.makeText(getContext(),"Không được để trống",Toast.LENGTH_SHORT).show();
-                        }else if(!(isValidFormat("dd/mm/yyyy",ed_birthday.getText().toString()))){
+                        }else if(!(isValidFormat("dd/MM/yyyy",ed_birthday.getText().toString()))){
                             Toast.makeText(getContext(),"Không đúng định dạng ngày",Toast.LENGTH_SHORT).show();
                         }
                         else {
