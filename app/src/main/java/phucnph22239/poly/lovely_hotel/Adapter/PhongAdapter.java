92c1399b8e8bae58d467c762908afbe733835a59
nguyenhungthe2022 <thenhph22409@fpt.edu.vn>
2022-11-21 11:25:51 +0700

    chuc nang them phong

--- app/src/main/java/phucnph22239/poly/lovely_hotel/Adapter/PhongAdapter.java
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Adapter/PhongAdapter.java
@@ -1,4 +1,64 @@
 package phucnph22239.poly.lovely_hotel.Adapter;
 
-public class PhongAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import phucnph22239.poly.lovely_hotel.DAO.PhongDao;
+import phucnph22239.poly.lovely_hotel.DAO.loaiphongDAO;
+import phucnph22239.poly.lovely_hotel.DTO.Phong;
+import phucnph22239.poly.lovely_hotel.R;
+
+
+public class PhongAdapter extends RecyclerView.Adapter<PhongAdapter.phongViewHolder> {
+    private Context context;
+    private List<Phong> list;
+    private PhongDao phongDAO;
+    private loaiphong loaiphong;
+    private loaiphongDAO loaiphongDAO;
+
+    public PhongAdapter(Context context, List<Phong> list) {
+        this.context = context;
+        this.list = list;
+        phongDAO=new PhongDao(context);
+        loaiphongDAO=new loaiphongDAO(context);
+    }
+    @NonNull
+    @Override
+    public phongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_phong, parent, false);
+        return new phongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull phongViewHolder holder, int position) {
+        Phong phong=list.get(position);
+        holder.item_phong_ten.setText(""+phong.getName());
+        holder.item_phong_loaiphong.setText(""+loaiphong.);
+        holder.item_phong_giaphong.setText(""+phong.getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class phongViewHolder extends RecyclerView.ViewHolder{
+        TextView item_phong_ten,item_phong_loaiphong,item_phong_giaphong,item_phong_trangthai;
+        public phongViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item_phong_ten=itemView.findViewById(R.id.item_phong_ten);
+            item_phong_giaphong=itemView.findViewById(R.id.item_phong_giaphong);
+            item_phong_loaiphong=itemView.findViewById(R.id.item_phong_loaiphong);
+            item_phong_trangthai=itemView.findViewById(R.id.item_phong_trangthai);
+        }
+    }
 }
+
--- app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/FragmentPhong.java
+++ app/src/main/java/phucnph22239/poly/lovely_hotel/Fragment/FragmentPhong.java
@@ -1,74 +1,88 @@
 package phucnph22239.poly.lovely_hotel.Fragment;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import phucnph22239.poly.lovely_hotel.Adapter.PhongAdapter;
+import phucnph22239.poly.lovely_hotel.DAO.PhongDao;
+import phucnph22239.poly.lovely_hotel.DAO.loaiphongDAO;
+import phucnph22239.poly.lovely_hotel.DTO.Phong;
 import phucnph22239.poly.lovely_hotel.R;
 
 
 public class FragmentPhong extends Fragment {
-
-    FloatingActionButton fab;
-
-    public FragmentPhong() {
-        // Required empty public constructor
-    }
-
-    public static FragmentPhong newInstance() {
-        FragmentPhong fragment = new FragmentPhong();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+    private RecyclerView recyclerView;
+    private PhongDao phongDAO;
+    private loaiphongDAO loaiphongDAO;
+    private List<Phong> list=new ArrayList<Phong>();
+    private PhongAdapter adapter;
+    private FloatingActionButton button;
+    private TextInputEditText dialog_ed_tenphong,dialog_ed_giaphong;
+    private Spinner dialog_spn_loaiphong;
+    private CheckBox ckb_phong;
+    Button dialog_btn_themphong,dialog_btn_huythemphong;
+    PhongDao phong;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phong, container, false);
+        View view= inflater.inflate(R.layout.fragment_phong, container, false);
+        phongDAO=new PhongDao(getContext());
+        recyclerView=view.findViewById(R.id.rcv_phong);
+        list=phongDAO.getAll();
+        adapter=new PhongAdapter(getContext(),list);
+        button=view.findViewById(R.id.fab_add_phong);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendialog();
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+    private void opendialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_them_phong, null);
+        builder.setView(view);
+        Dialog dialog = builder.create();
+        dialog.show();
+        dialog_ed_tenphong=view.findViewById(R.id.dialog_ed_tenphong);
+        dialog_ed_giaphong=view.findViewById(R.id.dialog_ed_giaphong);
+        dialog_spn_loaiphong=view.findViewById(R.id.dialog_spn_loaiphong);
+        dialog_btn_themphong=view.findViewById(R.id.dialog_btn_themphong);
+        dialog_btn_huythemphong=view.findViewById(R.id.dialog_btn_huythemphong);
+        ckb_phong=view.findViewById(R.id.ckb_phong);
+        dialog_btn_themphong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        dialog_btn_huythemphong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        fab = view.findViewById(R.id.fab_add_phong);
-
-        fab.setOnClickListener(v -> {
-            openDialog();
-
-        });
-    }
-
-    private void openDialog() {
-        final Dialog dialog = new Dialog(getActivity());
-        dialog.setContentView(R.layout.dialog_them_phong);
-        Button btn_add,btn_cancel;
-
-        btn_add = dialog.findViewById(R.id.dialog_btn_themphong);
-        btn_cancel = dialog.findViewById(R.id.dialog_btn_huythemphong);
-
-        btn_cancel.setOnClickListener(v -> {
-            dialog.dismiss();
-        });
-
-        btn_add.setOnClickListener(v -> {
-
-        });
-
-    }
 }
